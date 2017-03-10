2f8174ba823e6260c1173ecd3a6fd63f17d40c32
ssogome <ssogome@gmail.com>
2017-03-10 10:18:15 -0700

    Add files via upload

--- IndividualRadioButton.js
+++ IndividualRadioButton.js
@@ -7,20 +7,25 @@
 			$(":radio[value='Standard (catalog request)']").click(function(){
 					alert('radio    '  );
 					requestType($(this).next().text());
+					if(currentReqType != $(this).next().text() && $("input[title='line1-part Required Field']").val() != "")	return false;
 			});
 			$(":radio[value='Single part number(s)']").click(function(){
 					alert('radio    '  );
 					requestType($(this).next().text());
+				if(currentReqType != $(this).next().text() && $("input[title='line1-part Required Field']").val() != "")	return false;
 			});
 			$(":radio[value='Special Request (not in Catalog)']").click(function(){
 					alert('radio    '  );
 					requestType($(this).next().text());
+					if(currentReqType != $(this).next().text() && $("input[title='line1-part Required Field']").val() != "")	return false;
 			});
 			$(":radio[value='Non-M&E']").click(function(){
 					alert('radio    '  );
 					requestType($(this).next().text());
+					if(currentReqType != $(this).next().text() && $("input[title='line1-part Required Field']").val() != "")	return false;
 			});			
 			$(":radio[value='P-Card / QMS Tracking']").click(function(){
 					alert('radio    '  );
 					requestType($(this).next().text());
+					if(currentReqType != $(this).next().text() && $("input[title='line1-part Required Field']").val() != "")	return false;
 			});
